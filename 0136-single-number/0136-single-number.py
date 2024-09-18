02b381b6559ad2c9fd9cf792ca65b902e2a86cfe
Pranav Jothivel <44120295+pranavjothivel@users.noreply.github.com>
2024-09-18 00:24:03 -0400

    Time: 181 ms (29.51%), Space: 13.6 MB (90.89%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,12 +1,6 @@
 class Solution(object):
     def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        s = 0
-        # for num in nums:
-        #     solution = num ^ solution
+        res=0
         for i in nums:
-            s ^= i
-        return s
+            res^=i
+        return res
