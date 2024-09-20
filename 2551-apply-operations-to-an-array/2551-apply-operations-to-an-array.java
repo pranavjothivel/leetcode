7941f336112a041c32e5dd7987171b5134dd141c
Pranav Jothivel <44120295+pranavjothivel@users.noreply.github.com>
2024-09-20 00:40:57 -0400

    Time: 1 ms (95.34%), Space: 44.2 MB (17.48%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -1,26 +1,24 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
-        int n = nums.length;
+        int size = nums.length;
         
-        // Step 1: Apply the operation
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < size - 1; i++) {
             if (nums[i] == nums[i + 1]) {
                 nums[i] = nums[i] * 2;
                 nums[i + 1] = 0;
             }
         }
         
-        // Step 2: Shift non-zero elements to the front
-        int[] result = new int[n];
+        int[] array = new int[size];
         int index = 0;
         
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < size; i++) {
             if (nums[i] != 0) {
-                result[index++] = nums[i];
+                array[index++] = nums[i];
+                // index++;
             }
         }
         
-        // Return the array with zeros moved to the end
-        return result;
+        return array;
     }
 }
