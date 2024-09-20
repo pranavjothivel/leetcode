0fd5fae2500371433ca06a90c1318f20ce6ef752
Pranav Jothivel <44120295+pranavjothivel@users.noreply.github.com>
2024-09-19 23:43:42 -0400

    Time: 0 ms (100%), Space: 44.6 MB (63.96%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,15 +1,18 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        // left is also the counter for duplicates
         int left = 1;
 
         for (int right = 1; right < nums.length; right++) {
             // compare right number with number behind it
+            // comparing left to right has the edge case of going out of bounds
+            // on last index comparison
             if (nums[right] != nums[right - 1]) {
                 nums[left] = nums[right];
                 left++;
             }
         }
-
+        
         return left;
     }
 }
