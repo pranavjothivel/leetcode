5b7b8467fa352f94632c9ccdd12ec4e94404ff35
Pranav Jothivel <44120295+pranavjothivel@users.noreply.github.com>
2024-09-20 00:12:01 -0400

    Time: 2 ms (83.97%), Space: 45.4 MB (95.69%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int counter = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[counter] = nums[i];
+                counter++;
+            }
+        }
+
+        for (int i = counter; i < nums.length; i++) {
+            // counter is the number of zeroes in the array
+            // and the starting position from which to fill zeroes
+            nums[i] = 0;
+        }
+    }
+}
