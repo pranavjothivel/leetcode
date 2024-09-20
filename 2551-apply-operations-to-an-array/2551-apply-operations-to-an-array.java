c4010f83b5b418b23d3af33c1c95b6c26dbd8d8a
Pranav Jothivel <44120295+pranavjothivel@users.noreply.github.com>
2024-09-20 00:41:51 -0400

    Time: 1 ms (95.34%), Space: 44 MB (29.68%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -14,6 +14,7 @@ class Solution {
         
         for (int i = 0; i < size; i++) {
             if (nums[i] != 0) {
+                // array[index++] = nums[i];
                 array[index] = nums[i];
                 index++;
             }
