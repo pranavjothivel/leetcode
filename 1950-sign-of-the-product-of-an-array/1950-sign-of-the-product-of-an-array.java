a8f047fc4803c6287330662a046a1693d7833f5a
Pranav Jothivel <44120295+pranavjothivel@users.noreply.github.com>
2024-10-04 01:48:02 -0400

    Time: 0 ms (100%), Space: 44.4 MB (6.14%) - LeetHub

--- 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.java
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.java
@@ -3,7 +3,7 @@ class Solution {
         int productSign = 1;
         for (int num : nums) {
             if (num < 0) {
-                // ovverwrites productSign only if num is < 0
+                // overwrites productSign only if num is < 0
                 // and productSign becomes '1' when two negatives
                 // multiply and cancel signs
                 productSign *= -1;
