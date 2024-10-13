b956ad0bf8b43def6aa1fd5773247413e18197e1
Pranav Jothivel <44120295+pranavjothivel@users.noreply.github.com>
2024-10-12 22:42:08 -0400

    Time: 2 ms (61.47%), Space: 41.8 MB (10.39%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -3,12 +3,22 @@ class Solution {
         if (s.isEmpty()) {
             return true;
         }
-        int i = 0;
-        for (char c : t.toCharArray()) {
-            if (s.charAt(i) == c && ++i == s.length()) {
-                return true;
+        // int i = 0;
+        // for (char c : t.toCharArray()) {
+        //     if (s.charAt(i) == c && ++i == s.length()) {
+        //         return true;
+        //     }
+        // }
+        // return false;
+        int sCtr = 0;
+        int tCtr = 0;
+        
+        while (sCtr < s.length() && tCtr < t.length()) {
+            if (s.charAt(sCtr) == t.charAt(tCtr)) {
+                sCtr++;
             }
+            tCtr++;
         }
-        return false;
+        return (sCtr == s.length());
     }
 }
