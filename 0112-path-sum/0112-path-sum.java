940bf88a60890ce4c5c4be6258469308fb138448
Pranav Jothivel <44120295+pranavjothivel@users.noreply.github.com>
2024-10-14 15:12:30 -0400

    Time: 0 ms (100%), Space: 42.5 MB (97.5%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        // no sum if starting root node is null
+        if (root == null) {
+            return false;
+        }
+
+        // reached a leaf node
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val) ||
+                hasPathSum(root.right, targetSum - root.val);
+    }
+}
+
+/**
+    Root-to-leaf: From root node to leaf, search left and right
+    DFS problem
+ */
