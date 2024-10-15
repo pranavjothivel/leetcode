b016d7d82eaac4b01354fc764c460fd6409a4141
Pranav Jothivel <44120295+pranavjothivel@users.noreply.github.com>
2024-10-15 18:15:48 -0400

    Time: 0 ms (100%), Space: 40.7 MB (44.32%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,5 +1,15 @@
 class Solution {
+    // public int hammingWeight(int n) {
+    //     return Integer.bitCount(n);
+    // }
     public int hammingWeight(int n) {
-        return Integer.bitCount(n);
+        int bits = 0;
+        // n is unsigned
+        for (int i = 0; i < 32; i++) {
+            if (((n >> i) & 1) == 1) {
+                bits++;
+            }
+        }
+        return bits;
     }
 }
