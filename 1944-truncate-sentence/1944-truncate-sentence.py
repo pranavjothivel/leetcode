124b71fd51060dfc1bf5137e482c1ea8af261838
Pranav Jothivel <44120295+pranavjothivel@users.noreply.github.com>
2024-11-02 18:39:24 -0400

    Time: 4 ms (6.95%), Space: 16.5 MB (90.57%) - LeetHub

--- 1944-truncate-sentence/1944-truncate-sentence.py
+++ 1944-truncate-sentence/1944-truncate-sentence.py
@@ -1,9 +1,11 @@
 class Solution:
     def truncateSentence(self, s: str, k: int) -> str:
-        words = s.strip().split(' ')
-        newS = ''
-        for i in range(0, k):
-            newS += words[i]
-            if i != k - 1:
-                newS += ' '
-        return newS
+        # words = s.strip().split(' ')
+        # newS = ''
+        # for i in range(0, k):
+        #     newS += words[i]
+        #     if i != k - 1:
+        #         newS += ' '
+        # return newS
+
+        return " ".join(s.split(' ')[:k])
