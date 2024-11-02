a04d29736897da677da788271b4770a80baeda94
Pranav Jothivel <44120295+pranavjothivel@users.noreply.github.com>
2024-11-02 18:47:59 -0400

    Time: 15 ms (100%), Space: 16.6 MB (5.56%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,10 +1,21 @@
 class Solution:
-    def fib(self, n: int, memo={}) -> int:
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
+    # def fib(self, n: int, memo={}) -> int:
+    #     if n == 0:
+    #         return 0
+    #     elif n == 1:
+    #         return 1
 
-        if n not in memo:
-            memo[n] = self.fib(n - 1, memo) + self.fib(n - 2, memo)  # Recursive calls with memoization
-        return memo[n]
+    #     if n not in memo:
+    #         memo[n] = self.fib(n - 1, memo) + self.fib(n - 2, memo)
+    #     return memo[n]
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+        memo = [0, 0, 1]
+
+        for i in range(2, n + 1):
+            memo[0] = memo[1]
+            memo[1] = memo[2]
+            memo[2] = memo[0] + memo[1]
+
+        return memo[2]
