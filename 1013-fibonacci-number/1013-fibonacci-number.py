7af8d7ebddbaf33cba25847b990f4ebaa223c19c
Pranav Jothivel <44120295+pranavjothivel@users.noreply.github.com>
2024-11-02 18:50:32 -0400

    Time: 37 ms (56.84%), Space: 16.5 MB (69.85%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,21 +1,12 @@
 class Solution:
-    # def fib(self, n: int, memo={}) -> int:
-    #     if n == 0:
-    #         return 0
-    #     elif n == 1:
-    #         return 1
+    def fib(self, n: int, memo={}) -> int:
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
 
-    #     if n not in memo:
-    #         memo[n] = self.fib(n - 1, memo) + self.fib(n - 2, memo)
-    #     return memo[n]
-    def fib(self, n: int) -> int:
-        if n < 2:
-            return n
-        memo = [0, 0, 1]
+        if n not in memo:
+            memo[n] = self.fib(n - 1, memo) + self.fib(n - 2, memo)
+        return memo[n]
 
-        for i in range(2, n + 1):
-            memo[0] = memo[1]
-            memo[1] = memo[2]
-            memo[2] = memo[0] + memo[1]
-
-        return memo[2]
+# review how to do with dp and sliding window
