3e79812be309d79113e9eec34217b5b7ef0278b7
Pranav Jothivel <44120295+pranavjothivel@users.noreply.github.com>
2025-02-25 15:02:52 -0500

    Time: 3 ms (84.9%), Space: 17.9 MB (74.73%) - LeetHub

---
+++ 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        ans = inf
+
+        for n in nums:
+            # handles case if both have same absolute value
+            if abs(n) < abs(ans) or (abs(n) == abs(ans) and n > ans):
+                ans = n
+                # print(ans)
+        return ans
