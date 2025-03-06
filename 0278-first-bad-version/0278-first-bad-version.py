80026e6444c96bc97b1410af7c3ce01c3d05c172
Pranav Jothivel <44120295+pranavjothivel@users.noreply.github.com>
2025-03-06 00:37:00 -0500

    Time: 36 ms (67.79%), Space: 17.8 MB (43.73%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -3,13 +3,15 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        # binary search
-        left, right = 1, n
+        l, r = 1, n
 
-        while left < right:
-            mid = (left + right) >> 1 # midpt calc using bit shifting
+        while l < r:
+            mid = (l + r) >> 1
+
             if isBadVersion(mid):
-                right = mid
-            else:
-                left = mid + 1
-        return left
+                r = mid
+            else: 
+                l = mid + 1
+        return l
+
+        
