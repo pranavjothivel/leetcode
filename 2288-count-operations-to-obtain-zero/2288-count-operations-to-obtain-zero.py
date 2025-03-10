0578e2e07d36348883d119ef33cc738b91e619f4
Pranav Jothivel <44120295+pranavjothivel@users.noreply.github.com>
2025-03-10 12:15:12 -0400

    Time: 39 ms (62.07%), Space: 17.8 MB (52.59%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.py
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.py
@@ -5,8 +5,7 @@ class Solution:
         while (num1 != 0 and num2 != 0):
             if num1 >= num2:
                 num1 -= num2
-                ops +=1
             else:
                 num2 -= num1
-                ops += 1
+            ops += 1
         return ops
