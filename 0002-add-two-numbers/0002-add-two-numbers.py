2dc8df5358feda9754e8b1c7903c42bd675261cf
Pranav Jothivel <44120295+pranavjothivel@users.noreply.github.com>
2025-10-01 18:50:33 -0400

    Time: 0 ms (100%), Space: 17.9 MB (56.77%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,43 +5,23 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        stack = []
-        num1 = ""
-        num2 = ""
+        current = sum_list = ListNode(float('inf'))
+        carry = 0
+        while l1 or l2 or carry:
+            summ = carry
+            if l1:
+                summ += l1.val
+            if l2:
+                summ += l2.val
 
-        curr = l1
-        while curr:
-            stack.append(curr.val)
-            curr = curr.next
+            carry = summ // 10
+            node_val = summ % 10
+            
+            current.next = ListNode(node_val)
+            current = current.next
+
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+            
         
-
-        while len(stack) != 0:
-            num1 += str(stack.pop())
-        num1 = int(num1)
-
-        stack = []
-
-        curr = l2
-        while curr:
-            stack.append(curr.val)
-            curr = curr.next
-        
-
-        while len(stack) != 0:
-            num2 += str(stack.pop())
-        num2 = int(num2)
-
-        stack = []
-
-        sum = num1 + num2
-        sum_list = list(str(sum))[::-1]
-        
-        sum_node = ListNode(int(sum_list[0]))
-        prev = sum_node
-
-        for i in range(1, len(sum_list)):
-            new_node = ListNode(int(sum_list[i]))
-            prev.next = new_node
-            prev = new_node
-        
-        return sum_node
+        return sum_list.next
